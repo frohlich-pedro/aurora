6dba166fb4a76861f93ef7937c7ca20c0e3286da
frohlich-pedro <pedro_h_nf@outlook.com>
2025-05-13 07:57:41 -0300

    update - kernel.c i don't think that was needed, if it makes difference later it's just adding it back.

--- src/kernel.c
+++ src/kernel.c
@@ -1,14 +1,8 @@
 #include "drivers/vga_driver.h"
 
 void main(void) {
-    asm volatile("cli");
-
     clear_screen();
     print_kernel("Hello, World!\n", VGA_WHITE);
-
-    isr_install();
-    init_keyboard();
-
-    asm volatile ("sti");
-    while (1) asm volatile ("hlt");
+    
+    while (1);
 }
