346a0f93bed8f2e5075d7d7399895a0e22423968
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-14 08:53:54 -0300

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,14 +2,14 @@
 #include "drivers/kb_driver.h"
 
 void main(void) {
-    kcls();
-    kprintf("Keyboard test - press keys:\n", VGA_WHITE);
-    keyboard_install();
+  kcls();
+  kprintf("Keyboard test - press keys:\n", VGA_WHITE);
+  keyboard_install();
     
-    while (1) {
-        char c = keyboard_buffer_get();
-        if (c) {
-            kprintf("%c", c);  // Echo typed characters
-        }
+  while (1) {
+    char c = keyboard_buffer_get();
+    if (c) {
+      kprintf("%c", c);
     }
+  }
 }
