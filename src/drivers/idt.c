a4ec94cd7a2eadb7ba22a145c8ae4fb461678fdb
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-14 08:41:04 -0300

    update - i don't know why i did it that way...

--- src/drivers/idt.c
+++ src/drivers/idt.c
@@ -1,31 +1,30 @@
 #include "kb_driver.h"
 #include "../include/memory.h"
 
-struct idt_gate idt[IDT_ENTRIES];
-struct idt_ptr idtp;
+idt_gate idt[IDT_ENTRIES];
+idt_ptr idtp;
 
 void idt_init(void) {
-    idtp.limit = sizeof(struct idt_gate) * IDT_ENTRIES - 1;
-    idtp.base = (unsigned long)&idt;
+  idtp.limit = sizeof(struct idt_gate) * IDT_ENTRIES - 1;
+  idtp.base = (unsigned long)&idt;
     
-    // Initialize all IDT entries to zero
-    for (int i = 0; i < IDT_ENTRIES; i++) {
-        idt[i].offset_low = 0;
-        idt[i].selector = 0;
-        idt[i].zero = 0;
-        idt[i].type_attr = 0;
-        idt[i].offset_mid = 0;
-        idt[i].offset_high = 0;
-        idt[i].reserved = 0;
-    }
+  for (int i = 0; i < IDT_ENTRIES; i++) {
+    idt[i].offset_low = 0;
+    idt[i].selector = 0;
+    idt[i].zero = 0;
+    idt[i].type_attr = 0;
+    idt[i].offset_mid = 0;
+    idt[i].offset_high = 0;
+    idt[i].reserved = 0;
+  }
 }
 
 void idt_set_gate(unsigned char num, unsigned long base) {
-    idt[num].offset_low = base & 0xFFFF;
-    idt[num].selector = 0x08;  // Kernel code segment selector
-    idt[num].zero = 0;
-    idt[num].type_attr = IDT_PRESENT | IDT_DPL0 | IDT_GATE_INT;
-    idt[num].offset_mid = (base >> 16) & 0xFFFF;
-    idt[num].offset_high = (base >> 32) & 0xFFFFFFFF;
-    idt[num].reserved = 0;
+  idt[num].offset_low = base & 0xFFFF;
+  idt[num].selector = 0x08;  // Kernel code segment selector
+  idt[num].zero = 0;
+  idt[num].type_attr = IDT_PRESENT | IDT_DPL0 | IDT_GATE_INT;
+  idt[num].offset_mid = (base >> 16) & 0xFFFF;
+  idt[num].offset_high = (base >> 32) & 0xFFFFFFFF;
+  idt[num].reserved = 0;
 }
