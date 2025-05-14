c78f4eeb45ecf274f3679ac0d651015c2a1dae95
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-14 08:51:14 -0300

    Update io.c

--- src/include/io.c
+++ src/include/io.c
@@ -1,11 +1,11 @@
 #include "io.h"
 
 unsigned char byte_input(unsigned short port) {
-    unsigned char result;
-    asm volatile ("in %%dx, %%al" : "=a"(result) : "d"(port));
-    return result;
+  unsigned char result;
+  asm volatile ("in %%dx, %%al" : "=a"(result) : "d"(port));
+  return result;
 }
 
 void byte_output(unsigned short port, unsigned char data) {
-    asm volatile ("out %%al, %%dx" : : "a"(data), "d"(port));
+  asm volatile ("out %%al, %%dx" : : "a"(data), "d"(port));
 }
