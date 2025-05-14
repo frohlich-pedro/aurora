204645def34df0d7de34a67202913dec63f6b447
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-14 08:51:49 -0300

    Update memory.c

--- src/include/memory.c
+++ src/include/memory.c
@@ -1,15 +1,15 @@
 #include "memory.h"
 
 void memory_copy(char* dest, char* src, unsigned int bytes) {
-    unsigned int* d = (unsigned int*)dest;
-    unsigned int* s = (unsigned int*)src;
+  unsigned int* d = (unsigned int*)dest;
+  unsigned int* s = (unsigned int*)src;
 
-    int i;
-    for (i = 0; i < bytes / 4; i++) {
-        *(d + i) = *(s + i);
-    }
+  int i;
+  for (i = 0; i < bytes / 4; i++) {
+    *(d + i) = *(s + i);
+  }
 
-    for (i = (bytes & ~3); i < bytes; i++) {
-        *(dest + i) = *(src + i);
-    }
+  for (i = (bytes & ~3); i < bytes; i++) {
+    *(dest + i) = *(src + i);
+  }
 }
