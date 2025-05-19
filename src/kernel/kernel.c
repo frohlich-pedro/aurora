dbbf5212be0642987ffdcdb96d4ec157cf06c59a
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-19 10:03:37 -0300

    Update kernel.c

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,7 +3,6 @@
 #include "../cpu/timer.h"
 #include "../drivers/display.h"
 #include "../drivers/keyboard.h"
-
 #include "util.h"
 #include "mem.h"
 
@@ -16,13 +15,13 @@ void start_kernel() {
   init_keyboard();
   init_dynamic_mem();
 
-  int *ptr1 = alloc(5);
-  mem_free(ptr1);
+  int* ptr = alloc(5);
+  mem_free(ptr);
 
   print_string("> ");
 }
 
-void execute_command(char *input) {
+void execute_command(const char* input) {
   if (compare_string(input, "exit") == 0) {
     print_string("Stopping the CPU. Bye!\n");
     asm volatile("hlt");
