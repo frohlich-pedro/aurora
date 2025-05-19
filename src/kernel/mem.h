ea92d7f8c9bba2aa22ee01ec6cead0454a511177
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-19 10:01:30 -0300

    Update mem.h

--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -6,6 +6,6 @@ void memory_copy(unsigned char *source, unsigned char *dest, unsigned int nbytes
 void init_dynamic_mem();
 void print_dynamic_node_size();
 void print_dynamic_mem();
-void *mem_alloc(unsigned int size);
+void* mem_alloc(unsigned int size);
 void mem_free(void *p);
 void* alloc(int n);
