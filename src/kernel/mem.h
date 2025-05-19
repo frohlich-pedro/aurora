d9868f9bce70db92ffc703a9f86ce03a71f8259b
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-19 10:09:36 -0300

    Update mem.h

--- src/kernel/mem.h
+++ src/kernel/mem.h
@@ -2,7 +2,7 @@
 
 #define NULL_POINTER ((void*)0)
 
-void memory_copy(unsigned char *source, unsigned char *dest, unsigned int nbytes);
+void memory_copy(unsigned char* source, unsigned char* dest, unsigned int nbytes);
 void init_dynamic_mem();
 void print_dynamic_node_size();
 void print_dynamic_mem();
