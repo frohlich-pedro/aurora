33bd76ac553f5496e689449a1cd54e07456bac4f
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-05-22 20:48:54 -0300

    i'm non ironically so proud of this...

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -17,6 +17,20 @@ void start_kernel() {
 
   int* ptr = alloc(5);
   mem_free(ptr);
+
+  print_string("  --  Welcome to Aurora  --  \n\n");
+  print_string("+-----------------------------------------------------------------------------+\n");
+  print_string("|                                                                             |\n");
+  print_string("|  ,adPPYYba,  88       88  8b,dPPYba,   ,adPPYba,   8b,dPPYba,  ,adPPYYba,   |\n");
+  print_string("|  \"\"     `Y8  88       88  88P\'   \"Y8  a8\"     \"8a  88P\'   \"Y8  \"\"     `Y8   |\n");
+  print_string("|  ,adPPPPP88  88       88  88          8b       d8  88          ,adPPPPP88   |\n");
+  print_string("|  88,    ,88  \"8a,   ,a88  88          \"8a,   ,a8\"  88          88,    ,88   |\n");
+  print_string("|  `\"8bbdP\"Y8   `\"YbbdP\'Y8  88           `\"YbbdP\"\'   88          `\"8bbdP\"Y8   |\n");
+  print_string("|                                                                             |\n");
+  print_string("+-----------------------------------------------------------------------------+\n");
+  print_string("\n\nEnter \'help\' to see command list.\n\n");
   
   shell_init();
 }
+
+
