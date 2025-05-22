a00c98b305e14fe51740a42ee2a16feca31114ac
frohlich-pedro <pedro_h_nf@outlook.com>
2025-05-22 16:02:39 -0300

    update - help

--- src/shell/bin/help.c
+++ src/shell/bin/help.c
@@ -5,7 +5,7 @@ void help(void) {
 	shell_command_t* end_commands = 5;
 	do {
 		print_string((p_commands++)->name);
-		print_string((p_commands++)->definition);
+		print_string((p_commands)->definition);
 		print_string("\n");
 	} while (p_commands < end_commands);
 }
