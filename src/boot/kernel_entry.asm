28829e5f309fbc2050c438284e71a5e3e8e372a2
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-06-07 16:59:28 -0300

    update - added multiboot header

--- src/boot/kernel_entry.asm
+++ src/boot/kernel_entry.asm
@@ -1,6 +1,13 @@
-global _start;
+global _start
 [bits 32]
 
+section .multiboot
+align 4
+  dd 0x1BADB002
+  dd 0x00
+  dd -(0x1BADB002 + 0x00)
+
+section .text
 _start:
   [extern start_kernel]
   call start_kernel
