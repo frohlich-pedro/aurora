2e6f4253ed71f3205a8e02239d17d0ab86861777
Pedro Fröhlich <pedro_h_nf@outlook.com>
2025-07-06 21:23:16 +0000

    Update init.c

--- src/init/init.c
+++ src/init/init.c
@@ -1,20 +1,13 @@
+#include "../kernel/isr.h"
+#include "../kernel/irq.h"
+#include "../kernel/idt.h"
+#include "../mm/memory.h"
+
 void main() {
-	unsigned char* vidmem_ptr = (unsigned char*) 0xb8000;
-	unsigned char* vidmem_end = vidmem_ptr + (80 * 25 * 2);
-cls:
-	if (vidmem_ptr < vidmem_end) {
-		*vidmem_ptr++ = ' ';
-		*vidmem_ptr++ = 0xf;
-		goto cls;
-	}
-	vidmem_ptr = (unsigned char*) 0xb8000;
-	const char* string = "Hello, World!";
-put:
-	if (*string) {
-		*vidmem_ptr++ = *string++;
-		*vidmem_ptr++ = 0xf;
-		goto put;
-	}
-hlt:
-	goto hlt;
+	idt_init();
+	isr_init();
+	irq_init();
+	__asm__ volatile ("sti");
+loop:
+	goto loop;
 }
